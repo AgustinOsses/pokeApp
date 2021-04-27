214e59c7d46153d36cd9381dc638c79978349075
AgustinOsses <ossesagus@gmail.com>
2021-04-27 20:02:52 -0300

    Se agrega pantalla de error

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="error">
+    <emty-state />
+    <nuxt-link to="/"> Go home </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.error {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 90vh;
+}
+</style>
